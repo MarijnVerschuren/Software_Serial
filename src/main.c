419fe3b02a4645f48d907237550933a2ab87f162
MarijnVerschuren <marijnverschuren3@gmail.com>
2023-05-29 22:13:44 +0200

    TX up to 576K baud

--- src/main.c
+++ src/main.c
@@ -7,60 +7,61 @@
 #include "tim.h"
 #include "sys.h"
 
-
 // GPIO config
 #define RX_PORT GPIOA
-#define RX_PIN	0
+#define RX_PIN	5
 #define TX_PORT GPIOA
-#define TX_PIN	1
+#define TX_PIN	6
+#define DBA_PORT GPIOA
+#define DBA_PIN	4
+#define DBB_PORT GPIOA
+#define DBB_PIN	3
 // UART config
+//#define BAUD			460800
+//#define BAUD			115200
 #define BAUD			9600
 #define OVERSAMPLING	16
 #define DATA_BITS		8
 
 
-uint8_t started = 0;
-uint8_t transmission = 0;
-uint16_t sample = 0;
-uint16_t sampled = 0;
-uint8_t rx_bits = 0;
-uint8_t rx = 0;
-
-uint8_t* buffer;
-uint8_t* ptr;
+uint8_t* rx_buffer;
+uint8_t* rx_ptr;
 
 // RX
-extern void TIM1_UP_TIM10_IRQHandler(void) {
-	TIM1->SR &= ~TIM_SR_UIF;
-	sample <<= 1;
-	sample |= GPIO_read(RX_PORT, RX_PIN);
-	if (++sampled == OVERSAMPLING) {
-		sampled = 0;
-		register uint8_t bit = (sample >> 7 & 0b111);
-		// if (bit != 0b111 && bit) { /* TODO: fix bit */ }
-		bit = bit == 0b111;
-		if (!transmission && !bit)			{ transmission = 1; return; }
-		if (rx_bits == DATA_BITS && bit)	{  // TODO: multiple stop bits
-			stop_TIM(TIM1); started = 0;
-			*ptr++ = rx; return;
-		} rx = ((rx << 1) | bit);
-	}
-}
-extern void EXTI0_IRQHandler(void) {
-	EXTI->PR = EXTI_PR_PR0;
-	if (!started) {
-		transmission = 0;
-		sampled = 0;
-		rx_bits = 0;
-		started = 1;
-		start_TIM(TIM1);
+void SUART_read_until(uint8_t** buffer, uint32_t size, uint8_t until) {
+	uint16_t uart_tick;
+	uint8_t j, rx = 0;
+	for (uint32_t i = 0; i < size; i++) {
+		while (GPIO_read(RX_PORT, RX_PIN)); uart_tick = TIM1->CNT;  // wait until start bit
+		GPIO_write(DBA_PORT, DBA_PIN, 1);
+		for (j = 0; j < 8; j++) {
+			while (uart_tick == TIM1->CNT); uart_tick = TIM1->CNT;
+			rx = (rx >> 1) | (GPIO_read(RX_PORT, RX_PIN) << 7);
+			GPIO_toggle(DBB_PORT, DBB_PIN);
+		}
+		while (uart_tick == TIM1->CNT); uart_tick = TIM1->CNT;
+		GPIO_write(DBA_PORT, DBA_PIN, 0);
+		if (GPIO_read(RX_PORT, RX_PIN)) {
+			*(*buffer)++ = rx;
+			if (rx == until) { return; }
+		}
 	}
 }
 // TX
-extern void TIM2_IRQHandler(void) {
-	TIM2->SR &= ~TIM_SR_UIF;
+void SUART_write(const uint8_t* buffer, uint32_t size) {
+	uint16_t uart_tick = TIM1->CNT; uint8_t j;
+	for (uint32_t i = 0; i < size; i++) {
+		while (uart_tick == TIM1->CNT); uart_tick = TIM1->CNT;
+		GPIO_write(TX_PORT, TX_PIN, 0);  // start bit
+		for (j = 0; j < 8; j++) {
+			while (uart_tick == TIM1->CNT); uart_tick = TIM1->CNT;
+			GPIO_write(TX_PORT, TX_PIN, (buffer[i] >> j) & 0b1);
+		}
+		while (uart_tick == TIM1->CNT); uart_tick = TIM1->CNT;
+		GPIO_write(TX_PORT, TX_PIN, 1);  // stop bit
+		while (TIM1->CNT - uart_tick < 3); uart_tick = TIM1->CNT;
+	}
 }
-
 
 int main(void) {
 	// SYS_CLOCK (100Mhz)
@@ -72,28 +73,35 @@ int main(void) {
 	sys_clock_init(sys_config); free(sys_config);
 
 	// GPIO
-	fconfig_GPIO(RX_PORT, RX_PIN, GPIO_input, GPIO_pull_up, GPIO_open_drain, GPIO_very_high_speed, 0);
-	fconfig_GPIO(TX_PORT, TX_PIN, GPIO_output, GPIO_pull_up, GPIO_open_drain, GPIO_very_high_speed, 0);
+	fconfig_GPIO(RX_PORT, RX_PIN, GPIO_input, GPIO_no_pull, GPIO_push_pull, GPIO_very_high_speed, 0);
+	fconfig_GPIO(TX_PORT, TX_PIN, GPIO_output, GPIO_no_pull, GPIO_push_pull, GPIO_very_high_speed, 0);
+	fconfig_GPIO(DBA_PORT, DBA_PIN, GPIO_output, GPIO_no_pull, GPIO_push_pull, GPIO_very_high_speed, 0);
+	fconfig_GPIO(DBB_PORT, DBB_PIN, GPIO_output, GPIO_no_pull, GPIO_push_pull, GPIO_very_high_speed, 0);
+	GPIO_write(TX_PORT, TX_PIN, 1);  // let the line be floating
 
 	// EXTI
-	config_EXTI(RX_PIN, RX_PORT, 1, 0);
+	config_EXTI(RX_PIN, RX_PORT, 1, 1);
 
 	// TIM
-	uint32_t rx_psc = (100000000 / (BAUD * OVERSAMPLING - 1));
-	uint32_t tx_psc = (50000000 / (BAUD - 1));
-	config_TIM(TIM1, rx_psc, 1);
-	config_TIM(TIM2, tx_psc, 1);
-	start_TIM_update_irq(TIM1);
-	start_TIM_update_irq(TIM2);
+	uint32_t rx_psc = (APB2_clock_frequency / (BAUD + 1));
+	// cycles_per_bit = (uint32_t)(F_CPU + speed / 2) / speed;
+	// https://www.deeptronic.com/software-design/writing-simple-software-serial-function-in-arduino/
+	config_TIM(TIM1, rx_psc, 0xffff);
+	start_TIM(TIM1);
 
 	// software serial
-	buffer =	malloc(100);
-	ptr =		buffer;
-	start_EXTI(RX_PIN);  // start software serial
+	rx_buffer =	calloc(100, 1);
+	rx_ptr =	rx_buffer;
 
 	// main loop
+	const uint8_t* message = "hello world!\n";
+	uint8_t message_length = 13;
 	for (;;) {
-		__NOP();
+		//while (tick - start < 10); start = tick;
+		SUART_write(message, message_length);
+		//SUART_read_until(&rx_ptr, 100, 0x00);
+		//rx_ptr = rx_buffer;
+		//__NOP();  // 891a0a121a0a121a0a121a0a121a0a121a0a121a0a121a0a121a0a121a0a121a0a121a0a121a0a121a0a121a0a121afc
 	}
 
 	// | baud	| OS  | sample_freq	|
